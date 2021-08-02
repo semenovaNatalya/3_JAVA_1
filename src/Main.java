25a726eb4c5982b9a4dc9da3604a7f22a937c50b
Semenova Natalya <semnazl@gmail.com>
2021-08-02 23:25:37 +0300

    error correction

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price;
-        int bonus_limit;
+        int ticketPrice;
+        int bonusLimit;
         int mile;
-        ticket_price = 5000;
-        bonus_limit = 20;
-        mile = ticket_price / bonus_limit;
-        if (ticket_price >= 20) {
-
-            System.out.println("Ваш бонус составляет");
-            System.out.println(mile);
-
-        } else {
-            System.out.println("Бонусы Вам не начислены");
-        }
+        ticketPrice = 600;
+        bonusLimit = 20;
+        mile = ticketPrice / bonusLimit;
+        System.out.println(mile);
 
     }
 }
